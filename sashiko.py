3fcf97f5467df4b71d4bb23b71dfdd805c270d86
Dmitrii Kiselev <dkiselev@google.com>
2026-05-19 14:11:39 +0200

    update sashiko.py

--- sashiko.py
+++ sashiko.py
@@ -3,15 +3,26 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHB
 from PyQt6.QtGui import QPainter, QColor, QPen
 from PyQt6.QtCore import Qt
 
+DEFAULT_V_BITS = 12
+DEFAULT_V_MASK = 693
+DEFAULT_H_BITS = 7
+DEFAULT_H_MASK = 17
+GRID_SIZE = 20
+LINE_THICKNESS = 3
+MIN_WINDOW_WIDTH = 450
+MIN_WINDOW_HEIGHT = 400
+INITIAL_WINDOW_WIDTH = 700
+INITIAL_WINDOW_HEIGHT = 600
+
 class DrawingArea(QWidget):
-    def __init__(self):
+    def __init__(self, on_resize_callback):
         super().__init__()
         self.setMinimumSize(400, 300)
-        self.v_bits = 12
-        self.v_mask = 693
-        self.h_bits = 7
-        self.h_mask = 17
-        self.grid_size = 20
+        self.v_bits = DEFAULT_V_BITS
+        self.v_mask = DEFAULT_V_MASK
+        self.h_bits = DEFAULT_H_BITS
+        self.h_mask = DEFAULT_H_MASK
+        self.on_resize_callback = on_resize_callback
 
     def set_data(self, v_bits, v_mask, h_bits, h_mask):
         self.v_bits = v_bits
@@ -20,6 +31,13 @@ class DrawingArea(QWidget):
         self.h_mask = h_mask
         self.update()
 
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        rect = self.rect()
+        cols = rect.width() // GRID_SIZE
+        rows = rect.height() // GRID_SIZE
+        self.on_resize_callback(cols, rows)
+
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
@@ -27,7 +45,7 @@ class DrawingArea(QWidget):
         rect = self.rect()
         painter.fillRect(rect, QColor("white"))
         
-        pen = QPen(QColor("black"), 3)
+        pen = QPen(QColor("black"), LINE_THICKNESS)
         painter.setPen(pen)
         
         v_binary = format(self.v_mask, f'0{self.v_bits}b')
@@ -43,55 +61,56 @@ class DrawingArea(QWidget):
         width = rect.width()
         height = rect.height()
         
-        current_x = self.grid_size
+        current_x = GRID_SIZE
         column_index = 0
         while current_x < width:
             bit_index = column_index % v_bits_count
             bit = v_binary[bit_index]
             
-            for y in range(0, height, self.grid_size):
-                segment_index = y // self.grid_size
+            for y in range(0, height, GRID_SIZE):
+                segment_index = y // GRID_SIZE
                 if bit == '0':
                     if segment_index % 2 == 0:
-                        painter.drawLine(current_x, y, current_x, min(y + self.grid_size, height))
+                        painter.drawLine(current_x, y, current_x, min(y + GRID_SIZE, height))
                 else:
                     if segment_index % 2 != 0:
-                        painter.drawLine(current_x, y, current_x, min(y + self.grid_size, height))
-            current_x += self.grid_size
+                        painter.drawLine(current_x, y, current_x, min(y + GRID_SIZE, height))
+            current_x += GRID_SIZE
             column_index += 1
 
-        current_y = self.grid_size
+        current_y = GRID_SIZE
         row_index = 0
         while current_y < height:
             bit_index = row_index % h_bits_count
             bit = h_binary[bit_index]
             
-            for x in range(0, width, self.grid_size):
-                segment_index = x // self.grid_size
+            for x in range(0, width, GRID_SIZE):
+                segment_index = x // GRID_SIZE
                 if bit == '0':
                     if segment_index % 2 == 0:
-                        painter.drawLine(x, current_y, min(x + self.grid_size, width), current_y)
+                        painter.drawLine(x, current_y, min(x + GRID_SIZE, width), current_y)
                 else:
                     if segment_index % 2 != 0:
-                        painter.drawLine(x, current_y, min(x + self.grid_size, width), current_y)
-            current_y += self.grid_size
+                        painter.drawLine(x, current_y, min(x + GRID_SIZE, width), current_y)
+            current_y += GRID_SIZE
             row_index += 1
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Sashiko Bit Mask Pattern")
-        self.setFixedSize(700, 600)
+        self.setMinimumSize(MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT)
+        self.resize(INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT)
 
         self.v_bits_input = QSpinBox()
         self.v_bits_input.setRange(1, 64)
-        self.v_bits_input.setValue(12)
+        self.v_bits_input.setValue(DEFAULT_V_BITS)
         self.v_bits_input.valueChanged.connect(self.update_binary_view)
 
         self.v_mask_input = QDoubleSpinBox()
         self.v_mask_input.setDecimals(0)
         self.v_mask_input.setRange(0, float(2**64 - 1))
-        self.v_mask_input.setValue(693)
+        self.v_mask_input.setValue(DEFAULT_V_MASK)
         self.v_mask_input.setSingleStep(1)
         self.v_mask_input.valueChanged.connect(self.update_binary_view)
 
@@ -100,20 +119,22 @@ class MainWindow(QMainWindow):
 
         self.h_bits_input = QSpinBox()
         self.h_bits_input.setRange(1, 64)
-        self.h_bits_input.setValue(7)
+        self.h_bits_input.setValue(DEFAULT_H_BITS)
         self.h_bits_input.valueChanged.connect(self.update_binary_view)
 
         self.h_mask_input = QDoubleSpinBox()
         self.h_mask_input.setDecimals(0)
         self.h_mask_input.setRange(0, float(2**64 - 1))
-        self.h_mask_input.setValue(17)
+        self.h_mask_input.setValue(DEFAULT_H_MASK)
         self.h_mask_input.setSingleStep(1)
         self.h_mask_input.valueChanged.connect(self.update_binary_view)
 
         self.h_result_view = QLineEdit()
         self.h_result_view.setReadOnly(True)
 
-        self.drawing_area = DrawingArea()
+        self.stats_label = QLabel()
+        
+        self.drawing_area = DrawingArea(self.update_stats)
 
         v_input_layout = QHBoxLayout()
         v_input_layout.addWidget(QLabel("V-Bits:"))
@@ -132,6 +153,7 @@ class MainWindow(QMainWindow):
         main_layout.addWidget(self.v_result_view)
         main_layout.addLayout(h_input_layout)
         main_layout.addWidget(self.h_result_view)
+        main_layout.addWidget(self.stats_label)
         main_layout.addWidget(self.drawing_area, 1)
 
         container = QWidget()
@@ -158,6 +180,10 @@ class MainWindow(QMainWindow):
         self.h_result_view.setText(f"Horizontal: {h_mask} = {h_binary}₂")
         
         self.drawing_area.set_data(v_bits, v_mask, h_bits, h_mask)
+
+    def update_stats(self, cols, rows):
+        total_squares = cols * rows
+        self.stats_label.setText(f"Grid size: {cols}x{rows} (Total squares fit: {total_squares})")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
